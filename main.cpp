99cd80fc3fdc9576831799074af7a6c7604da667
Fierralin <fierralin@hotmail.com>
2016-08-14 18:58:44 +0800

    origin

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#ifdef HAVE_UNISTD_H
+/* For isatty(). */
+#include <unistd.h>
+#else
+#include <io.h>
+#endif
+
+#if TIME_WITH_SYS_TIME
+# include <sys/time.h>
+# include <time.h>
+#else
+# if HAVE_SYS_TIME_H
+#  include <sys/time.h>
+# else
+#  include <time.h>
+# endif
+#endif
+
+#include "sgftree.h"
+
+int
+main(int argc, char *argv[])
+{
+  SGFTree sgftree;
+
+  //char *infilename = "form.SGF";
+  fprintf(stderr, "%d %s\n", argc, argv[1]); ////////////
+  char *infilename = argv[1];
+  //char *infilename = "tmp.sgf";
+
+  sgftree_clear(&sgftree);
+
+  sgftree_readfile(&sgftree, infilename);
+  printSGF(&sgftree);
+  //show_sgf_tree(sgftree.root);
+  //writesgf(sgftree.root, "last.sgf");
+  //sgftree.root->child;
+
+
+  sgfFreeNode(sgftree.root);
+
+  return 0;
+}  /* end main */
